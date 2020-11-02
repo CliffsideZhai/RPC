a491bc895cd9635958d7b33a5e9f7d9661caae70
Zhai-ShunkaiBest <875174996@qq.com>
2020-11-02 21:37:36 +0800

    "创建客户端"

---
+++ SpirngCloud/Eureka/src/main/java/com/cliffisde/DemoApplication1.java
@@ -0,0 +1,15 @@
+package com.cliffisde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication1.class, args);
+    }
+
+}
